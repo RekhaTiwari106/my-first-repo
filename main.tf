8598df5c634b370636d3234894afb0f29179e728
RekhaTiwari106 <diwedirekha106@gmail.com>
2025-07-20 13:33:54 +0530

    added all my git gile

---
+++ main.tf
@@ -0,0 +1,18 @@
+
+
+resource "azurerm_resource_group" "example" {
+  name     = "example"
+  location = "West Europe"
+}
+resource "azurerm_storage_account" "example" {
+  name                     = var.name
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+variable "name" {
+  type = string
+  default = "rdcfdfvfgffg"
+  
+}
